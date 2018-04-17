82250b6caa05051a287dfbaa6bd58325de8c9116
Yifan Lou <ylou4@stevens.edu>
2018-04-16 23:02:18 -0400

    Create index1.js

---
+++ 04-16/index1.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Header extends Component {
+    renderGoodWord(goodWord, badWord){
+        const isGoodWord = true;
+        return isGoodWord? goodWord:badWord
+    }
+    render(){
+        const word = 'world'
+        const classname = 'header'
+        return(
+           <div className = {classname}>
+            <h1> Hello {word}! </h1>
+            <h2>
+                {this.renderGoodWord(
+                    <strong> goodWord </strong>,
+                    <span> badWord </span>
+                )}
+            </h2>
+           </div>
+        )
+    }
+}
+
+ReactDOM.render(<Header/>, document.getElementById('root'));
+
